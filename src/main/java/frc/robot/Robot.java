5887862a3f6cec5ae9b1961648a866ab3d4d77f8
Phil Olinger <philip.olinger@gmail.com>
2022-04-01 19:49:00 -0400

    Preliminary winch support

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,16 +4,21 @@
 
 package frc.robot;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
-// import com.revrobotics.REVLibError;
-// import com.revrobotics.CANSparkMaxLowLevel.MotorType.*;
-// import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+// import edu.wpi.first.wpilibj.Joystick;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.SparkMaxPIDController;
+// import com.revrobotics.REVLibError;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType.*;
+import com.revrobotics.SparkMaxRelativeEncoder;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -33,13 +38,18 @@ public class Robot extends TimedRobot {
    */
   private CANSparkMax m_leftFrontMotor, m_leftRearMotor; 
   private CANSparkMax m_rightFrontMotor, m_rightRearMotor;
-  // Soon to include pickup roller motor and others
   private CANSparkMax m_intakeMotor;
+  private CANSparkMax m_winchMotorA, m_winchMotorB;
   private MotorControllerGroup m_leftMotors, m_rightMotors;
   private DifferentialDrive m_robotDrive;
+  private boolean m_intake_on;  // Keep track of intake toggled on/off
   private XboxController m_stick;
   private XboxController n_stick;
   private final Timer m_timer = new Timer();
+  // Experimental PID control parameters
+  private SparkMaxRelativeEncoder m_encoder;
+  private SparkMaxPIDController m_PIDController;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -52,30 +62,51 @@ public class Robot extends TimedRobot {
      * 
      * CAN Bus device IDS: 
      * https://docs.wpilib.org/en/latest/docs/software/can-devices/can-addressing.html
+     * 
+     * CANSparkMax API documentation
+     * https://codedocs.revrobotics.com/java/com/revrobotics/package-summary.html
      */
+    // Instantiate all motors
     m_rightFrontMotor = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
     m_rightRearMotor = new CANSparkMax(6, CANSparkMaxLowLevel.MotorType.kBrushless);
     m_leftFrontMotor = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
     m_leftRearMotor = new CANSparkMax(7, CANSparkMaxLowLevel.MotorType.kBrushless);
     m_intakeMotor = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);    
-   
-    // For 2022, we may need to set the right-side motors to inverted
+    m_winchMotorA = new CANSparkMax(5, CANSparkMaxLowLevel.MotorType.kBrushless);
+    m_winchMotorB = new CANSparkMax(8, CANSparkMaxLowLevel.MotorType.kBrushless);
+    // Instantiate joysticks
+    m_stick = new XboxController(0);
+    n_stick = new XboxController(1);
+
+    /**
+    * CONFIGURE THE DRIVE MOTORS AND DRIVETRAIN
+    */
+    // For 2022, we may need to set one side's drive motors to inverted
     m_rightFrontMotor.setInverted(false);
     m_rightRearMotor.setInverted(false);
     m_leftFrontMotor.setInverted(true);
     m_leftRearMotor.setInverted(true);
-    //m_intakeMotor.setInverted(false);
 
     m_leftMotors = new MotorControllerGroup(m_leftFrontMotor, m_leftRearMotor);
     m_rightMotors = new MotorControllerGroup(m_rightFrontMotor, m_rightRearMotor);
-
     m_robotDrive = new DifferentialDrive(m_leftMotors, m_rightMotors);
 
-    // m_intakeMotor = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
+    /**
+     * CONFIGURE THE INTAKE MOTOR
+     */
+    m_intakeMotor.setInverted(false);
+    // Set CANSparkMax idle modes if needed (kCoast or kBrake)
+    m_intakeMotor.setIdleMode(IdleMode.kCoast); // Maybe this will solve the single click?
+    m_intake_on = false;  // Intake motor on/off toggle variable (used later)
 
-    m_stick = new XboxController(0);
-    n_stick = new XboxController(1);
-
+    /**
+     * CONFIGURE THE WINCH MOTORS
+     */
+    /**
+     *  Set one winch motor to follow the other, but since they are facing opposite
+     *  directions make it follow with inverted=true
+     */
+    m_winchMotorB.follow(m_winchMotorA, true);   
   }
 
   /**
@@ -152,25 +183,58 @@ public class Robot extends TimedRobot {
     m_robotDrive.tankDrive((y_sign * (y_val * y_val)),
                            (x_sign * (x_val * x_val)));
 
-    // Control the intake by toggling on and off
-    boolean intake_on = false;
+    // Control the intake by toggling on and off with right front bumper
     if(n_stick.getRightBumperPressed()){
-      if(intake_on == false){
+      if(m_intake_on == false){
         // Toggle on
         m_intakeMotor.set(0.25);  // Speed is 0.25
+        m_intake_on = true;
       } else {
         // Toggle off
-        m_intakeMotor.set(0.0);
+        m_intakeMotor.stopMotor();
+        m_intake_on = false;
       }
     };
 
+    // Control the winch
+    // TBD
 
+    /**
+     * Experimental PID Control - refer to:
+     * https://docs.wpilib.org/en/stable/docs/software/advanced-controls/introduction/pid-video.html
+     * https://github.com/REVrobotics/SPARK-MAX-Examples/blob/master/Java/Encoder%20Feedback%20Device/src/main/java/frc/robot/Robot.java
+     */    
+    /**
+     * PID equation coefficients - we will set via Ziegler-Nichols method
+     * 1. Set kI, kD to 0
+     * 2. Increase Kp until steady oscillation (not growing oscillation) 
+     *    This is Kc (critical gain) and Pc (period of oscillation)
+     * 3. Your final values should be: Kp = 0.6*Kc ; Ki = 2*Kp/Pc ; Kd = 0.125*Kp*Pc
 
+     * 
+    kP = 0.1; // Constant for proportional input
+    kI = 1e-4; // Constant for integral input
+    KD = 1; // Constant for derivative input
+    kIz = 0; // Constant for iZone (?))
+    kFF = 0; // Constant for Feed-Forward Gain (?)
+    kMinOutput = -1;
+    kMaxOutput = 1; 
+
+    m_encoder = m_winchMotorA.getEncoder(); 
+    m_PIDController = m_winchMotorA.getPIDController();
+    m_PIDController.setFeedbackDevice(m_encoder);
+    m_PIDController.setP(kP);
+    m_PIDController.setI(kI);
+    m_PIDController.setD(kD);
+    m_PIDController.setIZone(kIz);
+    m_PIDController.setFF(kFF);
+    m_PIDController.setOutputRange(kMinOutput, kMaxOutput);
+    /* */
   }
 
   @Override
   public void teleopExit() {
-    m_intakeMotor.set(0.0);
+    m_intakeMotor.stopMotor();
   }
 
   /** This function is called once when the robot is disabled. */
